a09f9c3f1de2634a5f5e319890e259a86e91ba89
chriswolf45 <55752149+chriswolf45@users.noreply.github.com>
2020-01-14 20:56:25 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,17 @@
+var canvas,backgroundImage,playerCount,database,form,player,game;
+var gameState = 0;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+game = new Game();
+game.getState();
+game.start();
+
+}
+
+function draw(){
+  
+  
+}
